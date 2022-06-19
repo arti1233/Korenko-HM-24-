47034c8d723bf80a3700276f4e2837c4e7ac1f81
Artsiom Korenko <artemich1827284@gmail.com>
2022-06-19 19:52:26 +0300

    Add request to server
    Add request to server and add data object

--- Korenko HM 24/Korenko HM 24/ViewController.swift
+++ Korenko HM 24/Korenko HM 24/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let APIKey = "52e0cef2e795eb013506ecb52fb1c280"
+    var nameCity = "Minsk"
+    var measurement = UnitsOfMeasurement.metric
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        requestToServer(nameCity: nameCity, measurement: measurement)
+        
     }
 
 
+    
+    
+// MARK: Metods
+    
+    // Запрос для получения данных
+    
+    func requestToServer(nameCity: String, measurement: UnitsOfMeasurement) {
+        if let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(nameCity)&appid=\(APIKey)\(measurement.description)") {
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = "GET"
+            urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            let dataTask = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+                if let data = data {
+                    let weather = try! JSONDecoder().decode(WeatherData.self, from: data)
+                    print(weather.temperature)
+                }
+            }
+            dataTask.resume()
+        }
+    }
+    
+    
+    
+    
 }
 
