47e4de773fdc7fa53a63940c642d0b4ea9031a10
Artsiom Korenko <artemich1827284@gmail.com>
2022-06-26 20:30:10 +0300

    Add google maps

--- Korenko HM 24/Korenko HM 24/ViewController /MapViewController.swift
+++ Korenko HM 24/Korenko HM 24/ViewController /MapViewController.swift
@@ -6,15 +6,27 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var viewForMaps: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
+    
+        
+        
     }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        let camera = GMSCameraPosition.camera(withLatitude: 54.029, longitude: 27.579, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: view.frame, camera: camera)
+        
+        view.addSubview(mapView)
+        
+    }
 
 
 }
