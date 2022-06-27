89c19ed83f8b05235e0fb82e1a0c595e0933024e
Artsiom Korenko <artemich1827284@gmail.com>
2022-06-27 15:48:47 +0300

    Add currentWeather and hourlyWeather in weatherView

--- Korenko HM 24/Korenko HM 24/ViewController /MapViewController.swift
+++ Korenko HM 24/Korenko HM 24/ViewController /MapViewController.swift
@@ -13,7 +13,7 @@ class MapViewController: UIViewController {
     private var apiProvider: RestAPIProviderProtocol!
     var wetherData: Current?
     var measurement = UnitsOfMeasurement.metric
-    var image: UIImage?
+    var image = UIImage()
     
     @IBOutlet weak var mainView: UIView!
     @IBOutlet weak var exitButton: UIButton!
@@ -27,11 +27,7 @@ class MapViewController: UIViewController {
         apiProvider = AlamofireProvider()
         mainView.layer.cornerRadius = 15
         mainView.isHidden = true
-        
-        
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
+      
         let camera = GMSCameraPosition.camera(withLatitude: 54.029, longitude: 27.579, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
         mapView.delegate = self
@@ -41,7 +37,18 @@ class MapViewController: UIViewController {
         
         
     }
-
+    
+    
+    
+    @IBAction func closeButton(_ sender: Any) {
+        mainView.isHidden = true
+    }
+    
+    
+    
+// MARK: Metods
+    
+    // получение данных для погоды
     func getWeatherByCoordinates(lat: Double, lon: Double, measurement: String) {
         apiProvider.getWeatherForCityCoordinates(lat: lat, lon: lon, measurement: measurement) { result in
             switch result {
@@ -54,41 +61,37 @@ class MapViewController: UIViewController {
         }
     }
     
-    @IBAction func closeButton(_ sender: Any) {
-        mainView.isHidden = true
-    }
-    
+    // запрос для получения картинки
     func getIconImage(API: String) {
         let api = API
         guard let apiURL = URL(string: api) else { return }
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: apiURL) { (data, response, error) in
             guard let data = data, let icon = UIImage(data: data), error == nil else { return }
-            DispatchQueue.global().async {
-                self.image = icon
-            }
+            self.image = icon
         }
         task.resume()
     }
     
 }
 
+
+// extention для отображения mainView
 extension MapViewController: GMSMapViewDelegate {
     func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
-        DispatchQueue.global().async {
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            guard let self = self else { return }
             self.getWeatherByCoordinates(lat: coordinate.latitude, lon: coordinate.longitude, measurement: self.measurement.description)
             guard let weather = self.wetherData,
                   let weatherDescription = weather.weather.first else { return }
             let description = weatherDescription.weatherDescription.description
-            let temp = weather.temp
-            let fellsLike = weather.feelsLike
             let API = "https://openweathermap.org/img/wn/\(weatherDescription.icon)@2x.png"
             self.getIconImage(API: API)
         
             
             DispatchQueue.main.async {
-                self.temperatureLabel.text = String(temp)
-                self.feelsLikeLabel.text = "Feels like \(fellsLike)"
+                self.temperatureLabel.text = String(weather.temp)
+                self.feelsLikeLabel.text = "Feels like \(weather.feelsLike)"
                 self.weatherDescriptionLabel.text = description
                 self.imageView.image = self.image
                 self.mainView.isHidden = false
--- Korenko HM 24/Korenko HM 24/ViewController /WeatherViewController.swift
+++ Korenko HM 24/Korenko HM 24/ViewController /WeatherViewController.swift
@@ -11,24 +11,48 @@ class WeatherViewController: UIViewController {
 
     private var apiProvider: RestAPIProviderProtocol!
     
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var feelsLikeLabel: UILabel!
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    
+    
+    @IBOutlet weak var collectionTable: UICollectionView!
+    
+    
+    
     
     
     
     var weatherData: WeatherData?
-    var nameCity = "Minsk"
+    var nameCity = String()
     var measurement = UnitsOfMeasurement.metric
+    var image = UIImage()
+    var hourly: [Hourly]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionTable.delegate = self
+        collectionTable.dataSource = self
+        collectionTable.register(UINib(nibName: myCollectionCell.key, bundle: nil), forCellWithReuseIdentifier: myCollectionCell.key)
+        
         apiProvider = AlamofireProvider()
-        
-        
-        getCoordinatesByName(name: nameCity)
-        
+      
         
     }
 
-
+    @IBAction func choseCity(_ sender: Any) {
+        alertControllerForChoseCity()
+        
+        
+    }
+    
+    
+    
+    
+    
+    
 // MARK: Metods
     
     func getCoordinatesByName(name: String) {
@@ -51,11 +75,119 @@ class WeatherViewController: UIViewController {
             switch result {
             case .success(let value):
                 self.weatherData = value
-                print(value.daily)
+                self.hourly = value.hourly
+                print(value.current)
             case .failure(let error):
                 print(error)
             }
         }
     }
+    
+    // Метод для alertController
+    
+    func alertControllerForChoseCity() {
+        let choseCityAlert = UIAlertController(title: "Chose city", message: "Enter the name of the city", preferredStyle: .alert)
+        choseCityAlert.addTextField { textField in
+            textField.placeholder = "Name city"
+        }
+        let cancelButton = UIAlertAction(title: "Cancel", style: .destructive)
+        let setPasswordButton = UIAlertAction(title: "OK", style: .cancel){ [weak self] _ in
+            guard let textField = choseCityAlert.textFields?[0],
+                  let text = textField.text,
+                  let self = self else { return }
+            let textWithoutWhitespace = text.trimmingCharacters(in: .whitespaces)
+            self.nameCity = textWithoutWhitespace
+            DispatchQueue.global().async { [weak self]  in
+                guard let self = self else { return }
+                self.getCoordinatesByName(name: self.nameCity)
+                DispatchQueue.main.async {
+                    self.changeMainView()
+                    self.collectionTable.reloadData()
+                }
+            }
+        }
+            
+            choseCityAlert.addAction(setPasswordButton)
+            choseCityAlert.addAction(cancelButton)
+            present(choseCityAlert, animated: true)
+    }
+    
+    
+    func changeMainView() {
+        guard let weatherData = weatherData,
+              let weatherdescription = weatherData.current.weather.first else { return }
+        let API = "https://openweathermap.org/img/wn/\(weatherdescription.icon)@2x.png"
+        getIconImage(API: API)
+
+        tempLabel.text = String(weatherData.current.temp)
+        feelsLikeLabel.text = "Feels like \(weatherData.current.feelsLike)"
+        weatherDescriptionLabel.text = weatherdescription.weatherDescription.description
+        imageView.image = image
+        
+    }
+    
+    func getIconImage(API: String) {
+        let api = API
+        guard let apiURL = URL(string: api) else { return }
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: apiURL) { (data, response, error) in
+            guard let data = data, let icon = UIImage(data: data), error == nil else { return }
+            self.image = icon
+        }
+        task.resume()
+    }
+    
+    func convertUnixType(dt: Double) -> String {
+        let date = Date(timeIntervalSince1970: dt)
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT") //Set timezone that you want
+        dateFormatter.locale = NSLocale.current
+        dateFormatter.dateFormat = "HH:mm" //Specify your format that you want
+        let strDate = dateFormatter.string(from: date)
+        return strDate
+    }
+    
+    
+    
+    
 }
 
+//extension WeatherViewController: UITableViewDataSource, UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        0
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        UITableViewCell()
+//    }
+//
+//
+//}
+
+
+extension WeatherViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        guard let hourly = hourly else { return 0 }
+        return hourly.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: myCollectionCell.key, for: indexPath) as? myCollectionCell else { return UICollectionViewCell() }
+        
+        guard let weatherHourly = hourly,
+              let weather = weatherHourly[indexPath.row].weather.first else { return UICollectionViewCell() }
+      
+        var hourly = weatherHourly[indexPath.row]
+        
+        cell.timeLabel.text = convertUnixType(dt: Double(hourly.dt))
+        cell.tempLabel.text = String(hourly.temp)
+        cell.layer.cornerRadius = 20 
+        return cell
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 150, height: 150)
+    }
+    
+}
